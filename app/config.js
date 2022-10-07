f8f8b257b563c45f2667b32a2ee13c3739611c9b
Shahina Rahman <shahinarahman101@gmail.com>
2022-10-07 22:31:17 +0100

    Added in more copy

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Whitehall taxonomy prototype test',
+  serviceName: 'Whitehall taxonomy prototype',
 
   // Default port that prototype runs on
   port: '3000',
